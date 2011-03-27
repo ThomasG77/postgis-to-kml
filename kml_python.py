a039839445c1388305d9facff5110d6ee0868b12
ThomasG <thomas_gratier@yahoo.fr>
2011-03-27 04:54:50 +0200

    Make kml_python.py compliant to pylint

--- kml_python.py
+++ kml_python.py
@@ -10,7 +10,8 @@ CREATE AGGREGATE textcat_all(
   initcond    = ''
 );
 
-http://stackoverflow.com/questions/43870/how-to-concatenate-strings-of-a-string-field-in-a-postgresql-group-by-query
+http://stackoverflow.com/questions/43870/
+how-to-concatenate-strings-of-a-string-field-in-a-postgresql-group-by-query
 
 """
 
@@ -23,19 +24,28 @@ username = "user"
 password = "pass"
 
 # Connect to an existing database
-conn = psycopg2.connect("host=" + host + " dbname=" + dbname + " user=" + username + " password=" + password)
+conn = psycopg2.connect("host=" + host + " dbname=" + dbname
+                        + " user=" + username + " password=" + password)
 
 # Open a cursor to perform database operations
 cur = conn.cursor()
 
 # Execute a command: this creates a new table
-cur.execute("SELECT '<?xml version=\"1.0\" encoding=\"UTF-8\"?><kml xmlns=\"http://earth.google.com/kml/2.1\"><Document>\n'||textcat_all(replace(replace(st_askml(the_geom),'<MultiGeometry>','<Placemark>'||'\n<name>'||'Departement '||code_dept||'</name>\n'||'<description>'||'Departement '||code_dept||'</description>\n'||'<MultiGeometry>'),'</MultiGeometry>','</MultiGeometry></Placemark>')||'\n')||'</Document></kml>' FROM departement")
-kml_string = cur.fetchone()
+cur.execute("SELECT '<?xml version=\"1.0\" encoding=\"UTF-8\"?>"
+            + "<kml xmlns=\"http://earth.google.com/kml/2.1\">"
+            + "<Document>\n'||textcat_all(replace(replace("
+            + "st_askml(the_geom),'<MultiGeometry>'"
+            + ",'<Placemark>'||"
+            + "'\n<name>'||'Departement '||code_dept||'</name>\n'||"
+            + "'<description>'||'Departement '||code_dept||'</description>\n'"
+            + "||'<MultiGeometry>'),'</MultiGeometry>','</MultiGeometry>"
+            + "</Placemark>')||'\n')||'</Document></kml>' FROM departement")
+kmlstring = cur.fetchone()
 
 f = open('france_python.kml', 'w')
 
-# print kml_string[0]
-f.write(kml_string[0])
+# print kmlstring[0]
+f.write(kmlstring[0])
 
 f.close()
 
